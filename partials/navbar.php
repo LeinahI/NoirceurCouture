36c056bb4c1d8ddc767240cf229b748d5e8d75f5
Leinah <gtpndn@gmail.com>
2024-05-13 22:24:19 +0900

    navbar offcanvas implemented

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,87 +1,112 @@
 <?php
 $currentFilename = basename($_SERVER['PHP_SELF']);
 ?>
-<nav class="navbar navbar-expand-lg bg-primary shadow"> <!--  sticky-top -->
+<style>
+    @media (max-width: 991px) {
+        #offcanvasNavbar2 {
+            width: 25% !important;
+        }
+    }
+
+    @media (max-width: 767px) {
+        #offcanvasNavbar2 {
+            width: 30% !important;
+        }
+    }
+    @media (max-width: 576px) {
+        #offcanvasNavbar2 {
+            width: 40% !important;
+        }
+    }
+</style>
+<nav class="navbar navbar-expand-lg bg-primary" aria-label="Offcanvas navbar large">
     <div class="container">
         <a href="../views/index.php" class="navbar-brand">
-            <img src="../assets/images/logo/NoirceurCouture_BK.png" height="30" alt="LOGO">
+            <img src="../assets/images/logo/NoirceurCouture_BK.png" height="30" alt="Logo">
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar2" aria-controls="offcanvasNavbar2">
+            <i class="fa-solid fa-bars"></i>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item" <?php if (in_array($currentFilename, ['storelist.php', 'verifyAccount.php', 'verifyEmailFinal.php', "reset.php", "resetPassword.php"])) echo 'hidden'; ?>>
-                    <a class="nav-link text-brown" href="../views/storelist.php">Collections</a>
-                </li>
-                <!-- myCart start -->
-                <li class="nav-item" <?php if (in_array($currentFilename, ['myCart.php', 'verifyAccount.php', 'verifyEmailFinal.php', "reset.php", "resetPassword.php"])) echo 'hidden'; ?>>
-                    <!-- user cart qty -->
-                    <a class="nav-link" href="../views/myCart.php"><i class="fa-solid fa-cart-shopping text-accent"></i>&nbsp;
-                        <?php if (isset($_SESSION['auth'])) {
-                            $cartQty = getCartQty();
-                        ?>
-                            <span class="itemCartQty text-accent"><?php echo $cartQty; ?></span>
-                        <?php } else { ?>
-                            <span class="itemCartQty text-accent"></span>
-                        <?php } ?>
-                    </a>
-                </li>
-                <!-- myCart end -->
-
-                <!-- Wish List start -->
-                <li class="nav-item" <?php if (in_array($currentFilename, ['myLikes.php', 'verifyAccount.php', 'verifyEmailFinal.php', "reset.php", "resetPassword.php"])) echo 'hidden'; ?>>
-                    <!-- user cart qty -->
-                    <a class="nav-link" href="../views/myLikes.php"><i class="fa-solid fa-heart text-accent"></i>&nbsp;
-                        <?php if (isset($_SESSION['auth'])) {
-                            $cartQty = getLikesQty();
-                        ?>
-                            <span class="itemCartQty text-accent"><?php echo $cartQty; ?></span>
-                        <?php } else { ?>
-                            <span class="itemCartQty text-accent"></span>
-                        <?php } ?>
-                    </a>
-                </li>
-                <!-- Wish List end -->
-
-                <?php
-                if (isset($_SESSION['auth'])) {
-                    $user = getUserDetails();
-                    $data = mysqli_fetch_array($user);
-                    $profilePic = $data['user_profile_image'];
-                ?>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle text-brown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src="../assets/uploads/userProfile/<?= ($profilePic) ? $profilePic : 'defaultProfile.jpg' ?>" height="20" width="20" class="rounded-circle" alt="">
-                            <?= $_SESSION['auth_user']['user_username'] ?>
+        <div class="offcanvas offcanvas-end bg-primary" tabindex="-1" id="offcanvasNavbar2" aria-labelledby="offcanvasNavbar2Label">
+            <div class="offcanvas-header">
+                <a href="../views/index.php" class="navbar-brand">
+                    <img src="../assets/images/logo/NoirceurCouture_BK.png" height="30" alt="Logo">
+                </a>
+            </div>
+            <div class="offcanvas-body">
+                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                    <li class="nav-item" <?php if (in_array($currentFilename, ['storelist.php', 'verifyAccount.php', 'verifyEmailFinal.php', "reset.php", "resetPassword.php"])) echo 'hidden'; ?>>
+                        <a class="nav-link text-brown" href="../views/storelist.php">Collections</a>
+                    </li>
+                    <!-- myCart start -->
+                    <li class="nav-item" <?php if (in_array($currentFilename, ['myCart.php', 'verifyAccount.php', 'verifyEmailFinal.php', "reset.php", "resetPassword.php"])) echo 'hidden'; ?>>
+                        <!-- user cart qty -->
+                        <a class="nav-link" href="../views/myCart.php"><i class="fa-solid fa-cart-shopping text-accent"></i>&nbsp;<span class="text-brown d-inline d-lg-none">Cart</span>
+                            <?php if (isset($_SESSION['auth'])) {
+                                $cartQty = getCartQty();
+                            ?>
+                                <span class="itemCartQty text-accent"><?php echo $cartQty; ?></span>
+                            <?php } else { ?>
+                                <span class="itemCartQty text-accent"></span>
+                            <?php } ?>
                         </a>
-                        <ul class="dropdown-menu bg-primary">
-                            <li><a class="dropdown-item text-brown" href="myOrders.php">My Purchase</a></li>
-                            <li><a class="dropdown-item text-brown" href="myAccount.php">My Account</a></li>
-                            <li><a class="dropdown-item text-brown" href="../models/logout.php">Log Out</a></li>
-                        </ul>
                     </li>
-                <?php
-                } else {
-                ?>
-                    <li class="nav-item" <?php if (in_array($currentFilename, ['verifyAccount.php', 'verifyEmailFinal.php', "reset.php", "resetPassword.php"])) echo 'hidden'; ?>>
-                        <a class="nav-link text-brown" href="../seller/seller-registration.php">Start Selling</a>
+                    <!-- myCart end -->
+
+                    <!-- Wish List start -->
+                    <li class="nav-item" <?php if (in_array($currentFilename, ['myLikes.php', 'verifyAccount.php', 'verifyEmailFinal.php', "reset.php", "resetPassword.php"])) echo 'hidden'; ?>>
+                        <!-- user cart qty -->
+                        <a class="nav-link" href="../views/myLikes.php"><i class="fa-solid fa-heart text-accent"></i>&nbsp;<span class="text-brown d-inline d-lg-none">Likes</span>
+                            <?php if (isset($_SESSION['auth'])) {
+                                $cartQty = getLikesQty();
+                            ?>
+                                <span class="itemCartQty text-accent"><?php echo $cartQty; ?></span>
+                            <?php } else { ?>
+                                <span class="itemCartQty text-accent"></span>
+                            <?php } ?>
+                        </a>
                     </li>
-                    <li class="nav-item" <?php if (in_array($currentFilename, ['verifyAccount.php', 'verifyEmailFinal.php', "reset.php", "resetPassword.php"])) echo 'hidden'; ?>>
-                        <a class="nav-link text-brown" href="../views/register.php">Register</a>
-                    </li>
-                    <li class="nav-item" <?php if (in_array($currentFilename, ['verifyAccount.php', 'verifyEmailFinal.php', "reset.php", "resetPassword.php"])) echo 'hidden'; ?>>
-                        <a class="nav-link text-brown" href="../views/login.php">Log in</a>
-                    </li>
-                <?php
-                }
-                ?>
-            </ul>
+                    <!-- Wish List end -->
+
+                    <?php
+                    if (isset($_SESSION['auth'])) {
+                        $user = getUserDetails();
+                        $data = mysqli_fetch_array($user);
+                        $profilePic = $data['user_profile_image'];
+                    ?>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle text-brown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <img src="../assets/uploads/userProfile/<?= ($profilePic) ? $profilePic : 'defaultProfile.jpg' ?>" height="20" width="20" class="rounded-circle" alt="">
+                                <?= $_SESSION['auth_user']['user_username'] ?>
+                            </a>
+                            <ul class="dropdown-menu bg-primary">
+                                <li><a class="dropdown-item text-brown" href="myOrders.php">My Purchase</a></li>
+                                <li><a class="dropdown-item text-brown" href="myAccount.php">My Account</a></li>
+                                <li><a class="dropdown-item text-brown" href="../models/logout.php">Log Out</a></li>
+                            </ul>
+                        </li>
+                    <?php
+                    } else {
+                    ?>
+                        <li class="nav-item" <?php if (in_array($currentFilename, ['verifyAccount.php', 'verifyEmailFinal.php', "reset.php", "resetPassword.php"])) echo 'hidden'; ?>>
+                            <a class="nav-link text-brown" href="../seller/seller-registration.php">Start Selling</a>
+                        </li>
+                        <li class="nav-item" <?php if (in_array($currentFilename, ['verifyAccount.php', 'verifyEmailFinal.php', "reset.php", "resetPassword.php"])) echo 'hidden'; ?>>
+                            <a class="nav-link text-brown" href="../views/register.php">Register</a>
+                        </li>
+                        <li class="nav-item" <?php if (in_array($currentFilename, ['verifyAccount.php', 'verifyEmailFinal.php', "reset.php", "resetPassword.php"])) echo 'hidden'; ?>>
+                            <a class="nav-link text-brown" href="../views/login.php">Log in</a>
+                        </li>
+                    <?php
+                    }
+                    ?>
+                </ul>
+            </div>
         </div>
     </div>
 </nav>
 
-<script>
+<!-- <script>
     // Event delegation for dropdown
     $('body').on('click', '.nav-link.dropdown-toggle', function(e) {
         e.preventDefault();
@@ -94,4 +119,4 @@ $currentFilename = basename($_SERVER['PHP_SELF']);
         var href = $(this).attr('href');
         window.location.href = href; // Redirect to the selected link
     });
-</script>
+</script> -->
